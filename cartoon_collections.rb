6d47bf9eca4e51148e355a5bd7c21128955bd87c
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 18:16:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks = ["crackers", "gouda", "thyme"]
-
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 end
