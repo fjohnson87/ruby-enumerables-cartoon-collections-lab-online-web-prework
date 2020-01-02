9ec1548b7a4be97affa07550b25d1b1fc68858cf
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 18:01:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,7 @@ def summon_captain_planet# code an argument here
   puts planeteer_calls.collect
 end
   puts planeteer_calls.capitalize
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
